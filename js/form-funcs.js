5d5e6b775183466eec13ad43207bf3e7d244d41e
Cat Stevens <catb0t@protonmail.ch>
2017-05-31 11:45:19 -0400

    add loading icon, fix input form js (integration soon)

--- js/form-funcs.js
+++ js/form-funcs.js
@@ -9,16 +9,34 @@ var formResizer = {
   },
 
   addItem: function () {
+    var newname =  'field-' + formResizer.counter;
     var newfield = "";
 
-    newfield += '<fieldset class"field" id="field-' + formResizer.counter +
-                '">\n<input type="text" style="width: 20em;"/>\n';
+    newfield = '<fieldset class="field" id="' + newname + '" name="' + newname + '">\n';
+
+    newfield += `
+        <label>
+          Item name:
+          <input type="text" name="` + newname + `-name" value=""/>
+        </label>
+        <label>
+          Description:
+          <input type="textarea" name="` + newname + `-desc" value=""/>
+        </label>
+        <label>
+          Price:
+          <input type="number" name="` + newname + `-price" value=""/>
+        </label>
+        <label>
+          Options?
+          <input type="checkbox" name="` + newname + `-options" value=""/>
+        </label>`;
 
     newfield += '<button type="button" onclick="javascript:formResizer.removeItem(' + formResizer.counter +
                 ');">Remove this item</button>\n';
 
     newfield += '</fieldset>';
-    document.getElementById("edit-form").innerHTML += newfield;
+    document.getElementById("edit-form").insertAdjacentHTML('beforeend', newfield);
     ++formResizer.counter;
   },
 
@@ -37,7 +55,15 @@ var formResizer = {
 }
 
 function formExtract(name) {
-  var x = $('form#' + name).serializeJSON();
-  console.log(x);
-  return x;
+  var items = {};
+  for (fs of document.getElementsByTagName("fieldset")) {
+    var res = {};
+    for (intag of fs.getElementsByTagName("input")) {
+      var val = intag.type === "checkbox" ? intag.checked : intag.value;
+      var nme = intag.name;
+      res[nme] = val;
+    }
+    items[fs.id] = res;
+  }
+  console.log(items)
 }
