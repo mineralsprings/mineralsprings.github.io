db576013d1fabde803eb5000d77fcb04de33b2f5
Cat Stevens <catb0t@protonmail.ch>
2017-10-05 13:47:10 -0400

    fix broken js

--- js/forms/order.js
+++ js/forms/order.js
@@ -1,5 +1,11 @@
 var formOrder = {
-  function writeOutMenu () {
+  writeOutMenu: function () {
+    var form_grid = document.getElementById("form-grid");
+
+    form_grid.style.display = "block";
+
+    var menu_json = formOrder.getRemoteMenuData();
+
     
   }
 
